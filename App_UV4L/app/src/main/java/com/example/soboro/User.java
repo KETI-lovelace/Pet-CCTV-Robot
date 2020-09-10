10b108ba9d9af836de544f26877f8bbd58da67e3
ChaeLinYeo <evergreen786@naver.com>
2020-09-10 22:48:35 +0900

    Android App

---
+++ App_UV4L/app/src/main/java/com/example/soboro/User.java
@@ -0,0 +1,31 @@
+package com.example.soboro;
+
+public class User {
+    public String name;
+    public String email;
+    public String token;
+
+    //비어 있는 생성자가 필요하다.
+    //DataSnapshot.getValue(User.class)
+    public User() {
+
+    }
+
+    public User(String name, String email, String token) {
+        this.name = name;
+        this.email = email;
+        this.token = token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
